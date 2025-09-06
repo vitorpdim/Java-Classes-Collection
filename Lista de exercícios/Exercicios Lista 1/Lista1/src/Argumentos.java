18f531dc94558e0b589e495c134f526956213d14
Victor <vitor.cynh@gmail.com>
2025-09-06 17:57:09 -0300

    Add files via upload

---
+++ Lista de exercícios/Exercicios Lista 1/Lista1/src/Argumentos.java
@@ -0,0 +1,5 @@
+public class Argumentos {
+    public static void main(String[] args) {
+        System.out.println("String[] args");
+    }
+}
---
+++ Lista de exercícios/Exercicios Lista 1/Lista1/src/OiTchau.java
@@ -0,0 +1,6 @@
+public class OiTchau {
+    public static void main(String[] args) {
+
+
+    }
+}
