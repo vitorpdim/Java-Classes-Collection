8c40dc0dec028225908e890305f2e8102748c57b
Victor <vitor.cynh@gmail.com>
2025-11-27 22:18:41 -0300

    Add files via upload

--- DeclaracaoVariaveis/DeclaracaoVariaveis.java
+++ DeclaracaoVariaveis/DeclaracaoVariaveis.java
@@ -20,33 +20,10 @@ do sexo <sexo>, está registrado com o salário de R$ <salario>
 		String rg = "559.997.999-9";
 		String sexo = "MASCULINO (M)";
 		
-		double salario = 99999999999.9;
+		double salario = 99999999999.999;
 	
 	System.out.printf("O Senhor(a) %s portador(a) do RG numero %s, nascido em %s, do sexo %s, esta registrado com o salario de R$ %f", nome, rg, idade, sexo, salario);
 	}
  
   
 }
-
-/*
-
----------------- Qual é a saída do seguinte código quando compilado e executado? Selecione a(s) alternativa( s) correta(s).
-																											
-1. class Questao1 {
-2. public static void main (String args[]) {
-3. int y = 0;
-4. int x = z = 1;
-5. System.out.println(y + “,” + x + “,” + z);
-6. }
-7. }
-
-a) Imprime 0,1,1
-b) Erro durante a compilação na linha 1
-c) Imprime 0,0,1
-d) Erro durante a compilação na linha 4
-e) Erro durante a compilação na linha 5
-
-    -------------------->> RESPOSTA: d) Erro durante a compilação na linha 4
-o z não esta sendo declarado
-
-*/
